a041e1cc5c335567625748310cff6dc0d803f604
Nick Nyburg <nick@Mac.lan>
2026-02-15 22:42:54 -0800

    Added signal handling with sigaction

--- speed.c
+++ speed.c
@@ -3,10 +3,13 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define STDIN_FD 0
 #define INPUT_BUF_SIZE 1024
 #define SPEED_MIN 0
+static volatile sig_atomic_t g_inc_pending = 0;
+static volatile sig_atomic_t g_dec_pending = 0;
 
 static void print_speed_change(int is_increase, int speed)
 {
@@ -18,6 +21,18 @@ static void print_speed_change(int is_increase, int speed)
     fflush(stdout);
 }
 
+static void handle_sigusr1(int signo)
+{
+    (void)signo;
+    g_dec_pending = 1;
+}
+
+static void handle_sigusr2(int signo)
+{
+    (void)signo;
+    g_inc_pending = 1;
+}
+
 int main(void)
 {
     int speed = 0;
@@ -26,10 +41,47 @@ int main(void)
     printf("PID=%d\n", (int)getpid());
     fflush(stdout);
 
+    struct sigaction sa1;
+    struct sigaction sa2;
+
+    sigemptyset(&sa1.sa_mask);
+    sa1.sa_flags = 0;
+    sa1.sa_handler = handle_sigusr1;
+
+    sigemptyset(&sa2.sa_mask);
+    sa2.sa_flags = 0;
+    sa2.sa_handler = handle_sigusr2;
+
+    if (sigaction(SIGUSR1, &sa1, NULL) != 0) {
+        perror("sigaction SIGUSR1");
+        return 1;
+    }
+
+    if (sigaction(SIGUSR2, &sa2, NULL) != 0) {
+        perror("sigaction SIGUSR2");
+        return 1;
+    }
+
     for (;;) {
+        while (g_inc_pending || g_dec_pending) {
+            if (g_inc_pending) {
+                g_inc_pending = 0;
+                speed++;
+                print_speed_change(1, speed);
+            }
+
+            if (g_dec_pending) {
+                g_dec_pending = 0;
+                if (speed > SPEED_MIN)
+                    speed--;
+                print_speed_change(0, speed);
+            }
+        }
         ssize_t nread = read(STDIN_FD, buf, sizeof(buf));
 
         if (nread < 0) {
+            if (errno == EINTR)
+                continue;
             perror("read");
             return 1;
         }
